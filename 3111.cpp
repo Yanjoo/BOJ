1819725a67bed0411525288b2b906dbadf5578f3
Yanjoo <hanjub3@gmail.com>
2020-03-22 04:19:01 +0900

    3111

---
+++ 3111.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <stack>
+
+using namespace std;
+
+string T, A;
+stack<char, int> L, R;
+
+int main()
+{
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+
+    cin >> A >> T;
+
+    int l = 0, r = A.size() - 1;
+    bool forward = true;
+
+
+    return 0;
+}
