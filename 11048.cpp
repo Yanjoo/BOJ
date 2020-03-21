6e84e995d2e095c293d7c9f83aae8fcd6552781c
Yanjoo <hanjub3@gmail.com>
2020-03-22 04:19:31 +0900

    11048

---
+++ 11048.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int N, M;
+int dp[1001][1001];
+
+int main()
+{
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+
+    cin >> N >> M;
+
+    for (int i=1; i<=N; i++)
+        for (int j=1; j<=M; j++) 
+            cin >> dp[i][j];
+
+    for (int i=1; i<=N; i++)
+        for (int j=1; j<=M; j++)
+            dp[i][j] += max(dp[i-1][j-1], max(dp[i-1][j], dp[i][j-1]));
+
+    cout << dp[N][M];
+
+    return 0;
+}
