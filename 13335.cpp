35e04b9c19bfd77bf7ba81b0a7bbd5ba484bd0e9
Yanjoo <hanjub3@gmail.com>
2020-04-01 20:25:43 +0900

    시뮬레이션

--- 13335.cpp
+++ 13335.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
-#include <queue>
+#include <stack>
 
 using namespace std;
 
 int n, w, L;
 int truck[1000];
-queue<pair<int,int>> q;
+stack<pair<int,int>> st;
 bool start[1000];
+int result;
 
 int main()
 {
@@ -14,23 +15,37 @@ int main()
     ios::sync_with_stdio(0);
 
     cin >> n >> w >> L;
-    for (int i=0, k; i<n; i++)
+    
+    for (int i=0; i<n; i++)
     {
-        cin >> k;
-        q.push({k,i});
-    }
-
-    while (!q.empty())
-    {
-        auto cur = q.front();
-        
-        if(L - cur.first >= 0 && !start[cur.second])
+        int weight;
+        cin >> weight;
+        if (st.empty())
+            st.push({weight, 1});
+        else
         {
-            L = L - cur.first;
-            q.pop();
+            if (st.top().first + weight <= L && 
+                st.top().second + 1 <= w)
+            {
+                weight += st.top().first;
+                int num = st.top().second + 1;
+                st.pop();
+                st.push({weight, num});
+            }
+            else
+            {
+                st.push({weight, 1});
+            }
         }
-
     }
+
+    while (!st.empty())
+    {
+        result += w / st.top().second;
+        st.pop();
+    }
+
+    cout << result + 1;
 
 
     return 0;
