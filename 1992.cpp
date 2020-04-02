b10e93e215d221860c2f0121a7c142f6b3501b40
Yanjoo <hanjub3@gmail.com>
2020-04-03 08:26:27 +0900

    분할 정복

--- 1992.cpp
+++ 1992.cpp
@@ -4,51 +4,35 @@ using namespace std;
 
 int N;
 char arr[65][65];
-int z, o;
 
-void check(int i, int j, int x, int y)
+bool check(int i, int j, int size)
 {
-    for (int a=i; a<=x; a++)
+    int o=0, z=0;
+    for (int a=i; a<i+size; a++)
     {
-        for (int b=j; b<=y; b++)
+        for (int b=j; b<j+size; b++)
         {
             if (arr[a][b] == '1') o++;
             else z++;
         }
     }
+
+    if (o && z) return false;
+    else return true;
 }
 
 void solve(int i, int j, int size)
 {
-    if (size == 1)
+    if (check(i, j, size)) cout << arr[i][j];
+    else
     {
-        cout << arr[i][j];
-        return;
+        cout << "(";
+        int n = size/2;
+        for (int x=0; x<2; x++)
+            for (int y=0; y<2; y++)
+                solve(i+n*x, j+n*y, n);
+        cout << ")";
     }
-
-    cout << "(";
-
-    z = o = 0;
-    check(i, j, i+size/2-1, j+size/2-1);
-    if (z && o) solve(i, j, size/2);
-    else cout << (o ? 1 : 0);
-
-    z = o = 0;
-    check(i, j+size/2, i+size/2-1, j+size/2+size/2-1);
-    if (z && o) solve(i, j+size/2, size/2);
-    else cout << (o ? 1 : 0);
-
-    z = o = 0;
-    check(i+size/2, j, i+size/2+size/2-1, j+size/2-1);
-    if (z && o) solve(i+size/2, j, size/2);
-    else cout << (o ? 1 : 0);
-
-    z = o = 0;
-    check(i+size/2, j+size/2, i+size/2+size/2-1, j+size/2+size/2-1);
-    if (z && o) solve(i+size/2, j+size/2, size/2);
-    else cout << (o ? 1 : 0);
-
-    cout << ")";
 }
 
 int main()
@@ -57,6 +41,8 @@ int main()
     ios::sync_with_stdio(0);
 
     cin >> N;
+    int o=0, z=0;
+
     for (int i=1; i<=N; i++)
     {
         for (int j=1; j<=N; j++)
